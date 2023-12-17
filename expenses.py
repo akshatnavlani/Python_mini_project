202114efcabb1167fe6658b538b9c98b42892300
amankhandelwaal <aman.khandelwal2005@gmail.com>
2023-12-17 12:31:02 +0530

    edit expense.py

--- expenses.py
+++ expenses.py
@@ -3,13 +3,13 @@ import pandas as pd
 import streamlit as st
 
 # Function to ADD expenses
-def add_expenses(user_id, income, expenses, description, date, category):
+def add_expenses(user_id, expenses, description, date, category):
     conn = sqlite3.connect('user_data.db')
     cursor = conn.cursor()
 
     try:
         cursor.execute("INSERT INTO expenses (user_id, income, expenses, description, date, category) VALUES (?,?,?,?,?)",
-                       (user_id, income, expenses, description, date, category))
+                       (user_id, expenses, description, date, category))
         conn.commit()
         conn.close()
         return True, "Expense added successfully."
@@ -24,22 +24,22 @@ def view_expenses(user_id, date=None):
     cursor = conn.cursor()
 
     if date:
-        cursor.execute("SELECT id, user_id, income, expenses, description, date, category FROM expenses WHERE user_id=? AND date=?", (user_id, date))
+        cursor.execute("SELECT id, user_id, expenses, description, date, category FROM expenses WHERE user_id=? AND date=?", (user_id, date))
     else:
-        cursor.execute("SELECT id, user_id, income, expenses, description, date, category FROM expenses WHERE user_id=?", (user_id,))
+        cursor.execute("SELECT id, user_id, expenses, description, date, category FROM expenses WHERE user_id=?", (user_id,))
 
     expenses = cursor.fetchall()
     conn.close()
     return expenses
 
 # Function to MODIFY expenses
-def modify_expenses(expense_id, income, expenses, description, date):
+def modify_expenses(expense_id, expenses, description, date):
     conn = sqlite3.connect("user_data.db")
     cursor = conn.cursor()
 
     try:
         cursor.execute("UPDATE expenses SET income=?, expenses=?, description=?, date=? WHERE id=?",
-                       (income, expenses, description, date, expense_id))
+                       (expenses, description, date, expense_id))
         conn.commit()
         conn.close()
         return True, "Expense modified successfully."
@@ -51,7 +51,7 @@ def modify_expenses(expense_id, income, expenses, description, date):
 def display_expenses(expenses, title):
     if expenses:
         st.header(title)
-        df = pd.DataFrame(expenses, columns=["Expense ID", "User ID", "Income","Expenses", "Description", "Date", "Category"])
+        df = pd.DataFrame(expenses, columns=["Expense ID", "User ID", "Expenses", "Description", "Date", "Category"])
         st.dataframe(df)
     else:
         st.info("No expenses found for the selected period.")
